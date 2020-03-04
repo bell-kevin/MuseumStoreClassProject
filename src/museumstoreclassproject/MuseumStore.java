3a46a2c37a86d16ac09e086edb7ce7e47812ff5c
JCBoyd <jcboyd@pm.me>
2020-03-04 09:50:24 -0700

    Create repository, commit, and push to remote

---
+++ src/museumstoreclassproject/MuseumStore.java
@@ -0,0 +1,17 @@
+package museumstoreclassproject;
+
+/******************************************************************************
+ * MuseumStore.java 
+ * Programmer: @author jcboyd
+ *
+ * Class associated with MuseumStoreClassProject.java
+ *****************************************************************************/
+
+public class MuseumStore 
+{
+    
+    
+    
+    
+    
+} //end class MuseumStore
---
+++ src/museumstoreclassproject/MuseumStoreClassProject.java
@@ -0,0 +1,20 @@
+package museumstoreclassproject;
+
+/******************************************************************************
+ * MuseumStoreClassProject.java 
+ * Programmer: @author jcboyd
+ * Version: 1.0
+ * Course: SDEV 2210
+ *
+ * This program simulates sales at a museum store
+ *****************************************************************************/
+
+public class MuseumStoreClassProject {
+
+    public static void main(String[] args) {
+
+        
+        
+    } //end main
+    
+} //end class MuseumStoreClassProject
